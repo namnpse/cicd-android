4c44755f3ceb6d0fa35898202f3ff17665d47d7b
Nam Nguyen <nam444479@gmail.com>
2023-03-01 23:54:51 +0700

    Set label text test result

--- app/src/main/java/vn/namnp/retirementcalculator/MainActivity.kt
+++ app/src/main/java/vn/namnp/retirementcalculator/MainActivity.kt
@@ -2,10 +2,46 @@ package vn.namnp.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "2511983c-7caf-4a3b-b2e2-ed805eb57afb", Analytics::class.java, Crashes::class.java)
+
+        calculateButton.setOnClickListener {
+//             Crashes.generateTestCrash()
+            try {
+                val interestRate = interestEditText.text.toString().toFloat()
+                val currentAge = ageEditText.text.toString().toInt()
+                val retirementAge = retirementEditText.text.toString().toInt()
+                val monthly = monthlySavingsEditText.text.toString().toFloat()
+                val current = currentEditText.text.toString().toFloat()
+
+                val properties:HashMap<String, String> = HashMap()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
+
+                resultTextView.text = "At the current rate of $interestRate%, saving \$$monthly a month by $retirementAge."
+            } catch(ex: Exception){
+                Analytics.trackEvent(ex.message)
+            }
+        }
     }
 }
